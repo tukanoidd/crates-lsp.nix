14fc2286ec3c764a372f6dc000abdb046463f78a
Vitalii Lukyanov <tukanoid@outlook.com>
2025-11-01 17:17:47 +0100

    migrate to crane

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 {
+  description = "crates-lsp";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nci = {
-      url = "github:yusdacra/nix-cargo-integration";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
 
     crates-lsp = {
       url = "github:MathiasPius/crates-lsp";
@@ -17,25 +17,78 @@
   };
 
   outputs = inputs @ {
-    parts,
-    nci,
+    nixpkgs,
+    crane,
+    rust-overlay,
+    flake-utils,
     ...
   }:
-    parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-linux"];
-      imports = [
-        nci.flakeModule
-        ./crates.nix
-      ];
-      perSystem = {
-        pkgs,
-        config,
-        ...
-      }: let
-        crateOutputs = config.nci.outputs."crates-lsp";
+    (flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource inputs.crates-lsp;
+          strictDeps = true;
+        };
+
+        crates-lsp = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
       in {
-        devShells.default = crateOutputs.devShell;
-        packages.default = crateOutputs.packages.release;
+        checks = {
+          inherit crates-lsp;
+        };
+
+        packages = {
+          inherit crates-lsp;
+          default = crates-lsp;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = crates-lsp;
+        };
+      }
+    ))
+    // {
+      homeModules = {
+        default = {
+          config,
+          pkgs,
+          lib,
+          ...
+        }: let
+          crates-lsp-program = config.programs.crates-lsp;
+        in
+          with lib; {
+            options = {
+              programs.crates-lsp = {
+                enable = mkEnableOption "crates-lsp";
+                package = mkOption {
+                  description = "Package for crates-lsp";
+                  example = false;
+                  type = types.package;
+                };
+              };
+            };
+            config = {
+              home = {
+                packages = (
+                  if crates-lsp-program.enable
+                  then [crates-lsp-program.package]
+                  else []
+                );
+              };
+            };
+          };
       };
     };
 }
